e893ec4e4de9947fa5ff6a53f4bde512e3cdf9b9
rajatporwal <rajatporwal1483@gmail.com>
2019-11-10 23:34:26 +0530

    Reverse string

---
+++ q-array.js
@@ -0,0 +1,26 @@
+function p(a) {
+    console.log(a);
+  }
+  
+  // remove duplicate object from array.
+  
+  var  books = [ 
+          { title: "C++", author: "Bjarne" }, 
+          { title: "Java", author: "James" }, 
+          { title: "Python", author: "Guido" }, 
+          { title: "Java", author: "James" }, 
+        ];
+  
+  // 1.  Converting the array to a Set to remove the duplicates: 
+  
+  // JSON.stringify will convert objects into string and assign that values to array.
+  let jsonObject = books.map(JSON.stringify); 
+  
+  // since array has now 4 values as string set will remove equal string.
+  let uniqueSet = new Set(jsonObject); 
+  
+  let uniqueArray = Array.from(uniqueSet).map(JSON.parse);
+  
+  // 2. Using one of the keys as index:
+  
+  
