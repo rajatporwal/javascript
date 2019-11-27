205a5ab65d4b064b9d12979ea6a3adc8a9f2c523
rajatporwal <rajatporwal1483@gmail.com>
2019-11-27 19:30:35 +0530

    Class in javascript

---
+++ concepts/Class.js
@@ -0,0 +1,13 @@
+class Student {
+    constructor(age, name, roll) {
+      this.age = age;
+      this.name = name;
+      this.roll = roll;
+    }
+  }
+  
+  let s = new Student(10, 'rajat', 112);
+  
+  console.log(s.age)
+  console.log(s.name)
+  console.log(s.roll)
